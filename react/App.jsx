e27b55a2c2d5f11202c52ceffc4ddd0ab23c2073
darking09 <ftorres@mdsdigital.com>
2017-07-16 22:49:16 -0500

    Ajuste de wifi
    Se captura las redes por un script, se monta en el servidor y se ejecuta

--- react/App.jsx
+++ react/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container, Row, Col  } from 'reactstrap';
+import axios from 'axios';
 
 class App extends React.Component {
    render() {
@@ -21,13 +22,14 @@ class Header extends React.Component {
 			isOpen: false
 		};
 	}
+	
 	toggle() {
 		this.setState({
 			isOpen: !this.state.isOpen
 		});
 	}
 
-   render() {
+   	render() {
       return (
 		<div>
 			<Navbar color="faded" light toggleable>
@@ -67,20 +69,79 @@ class Content extends React.Component {
 }
 
 class FormStyle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.changeSSID = this.changeSSID.bind(this);
+		this.changePassword = this.changePassword.bind(this);
+		this.sendForm = this.sendForm.bind(this);
+
+		this.state = {
+			ssids: [],
+			ssid: '',
+			pw: ''
+		};
+	}
+
+	getSSID(){
+
+		axios.get('/ssid')
+		.then(res => {
+			const ssids = res.data.map((obj) => obj);
+			this.setState({ ssids: ssids });
+		});
+	}
+	
+	componentDidMount() {
+    	this.getSSID();
+  	}
+
 	render() {
+		let ssids = this.state.ssids.map((ssid) => <option value={ssid}>{ssid}</option>);
+
 		return (
-			<Form>
+			<Form onSubmit={this.sendForm}>
 				<FormGroup>
 					<Label for="SSID">Nombre de la Red</Label>
-		     		<Input type="text" name="SSID" id="SSID" />
+		     		<Input type="select" name="SSID" id="SSID" value={this.state.ssid} onChange={this.changeSSID}>
+						<option value=""></option>
+		     			{ssids}
+		     		</Input>
 		     	</FormGroup>
 		     	<FormGroup>
           			<Label for="password">Password</Label>
-		     		<Input type="password" name="password" id="password" placeholder="password" />
+		     		<Input type="password" name="password" id="password" placeholder="password" onChange={this.changePassword} value={this.state.pw} />
         		</FormGroup>
-        		<Button>Actualizar y resetear</Button>
+        		<Button type="submit">Actualizar y resetear</Button>
 			</Form>
 		);
+	}
+
+	sendForm(event){
+		event.preventDefault();
+
+		axios({
+			method: 'post',
+			url: '/ssid',
+			data: {
+				ssid: this.state.ssid,
+				pw: this.state.pw
+			},
+			responseType: 'json'
+		})
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+	}
+
+	changeSSID(event){
+		this.setState({ ssid: event.target.value });
+	}
+
+	changePassword(event){
+		this.setState({ pw: event.target.value });
 	}
 }
 
