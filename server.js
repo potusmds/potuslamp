cebb6c453cb19b96fc639370200920fcd6e1791d
darking09 <ftorres@mdsdigital.com>
2017-07-17 13:10:36 -0500

    Ajuste de reset.sh

--- server.js
+++ server.js
@@ -6,6 +6,5 @@ var server = require('./app');
 var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 
 server.listen(port, function() {
-  // eslint-disable-next-line
   console.log('Server running on port: %d', port);
 });
